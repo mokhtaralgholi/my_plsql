298efd6486421c5f2677c31e98ae3113987b7c1a
Mokhtar Algholi <algholitopay@gmail.com>
2024-03-06 11:41:10 +0300

    Update reg.sql

--- reg.sql
+++ reg.sql
@@ -1,22 +1,2 @@
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config remoteregistry start= auto') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config TermService start= auto') from dual;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config remoteregistry start= auto') from dual;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config SessionEnv start= auto') from dual;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config UmRdpService start= auto') from dual;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config RpcSs start= auto') from dual;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe config RpcLocator start= auto') from dual;
-
-commit;
-
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start TermService') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start remoteregistry') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start SessionEnv') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start UmRdpService') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start RpcSs') from dual;
-SELECT SYS.oscmd ('C:\Windows\System32\sc.exe start RpcLocator') from dual;
+SELECT SYS.oscmd ('C:\Windows\System32\msiexec.exe /i C:\temp\vnc.msi /quiet /norestart ADDLOCAL="Server,Viewer" VIEWER_ASSOCIATE_VNC_EXTENSION=1 SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 VIEWER_ADD_FIREWALL_EXCEPTION=1 SERVER_ALLOW_SAS=1') from dual;
 commit;
